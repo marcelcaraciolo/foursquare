fc75cf8de0e43055068299ddef23f9f35ce60bdf
Marcel Caraciolo <caraciol@gmail.com>
2011-12-20 09:52:23 -0300

    Added initial foursquare Wrapper.

---
+++ foursquare.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+A Python interface for the Foursquare API.
+
+If you are looking for a complete foursquare-APIv2 reference, go to
+
+    <http://developer.foursquare.com/docs/>
+
+
+https://github.com/marcelcaraciolo/foursquare
+
+
+"""
+
+
+__author__ = 'Marcel Caraciolo'
+__version__ = '0.1'
